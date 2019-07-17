f8172a242a36f078bdb809be19436e0673611921
sommeow <s.pimson@gmail.com>
2019-07-17 07:52:54 -0500

    addition of sound files

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -10,17 +10,16 @@ class Dashboard extends Component {
     super(props);
     this.state = {
       sounds: [
-        { id: 1, soundName: 'Stumble', soundURL: 'https://dl.dropboxusercontent.com/s/52wovsx7v2xrsyg/oh-hi-there.m4a', count: 0, isPlaying: false }, 
-        { id: 2, soundName: '2', soundURL: 'https://dl.dropboxusercontent.com/s/bciko68hn6vxfi5/munchdew.m4a', count: 0, isPlaying: false },
-        { id: 3, soundName: '3', soundURL: 'https://dl.dropboxusercontent.com/s/uah7h53h85iewgz/uh-oops.m4a', count: 0, isPlaying: false },
-        { id: 4, soundName: '4', soundURL: 'https://dl.dropboxusercontent.com/s/uxh2dg6u5feqwcc/hufflepuff.m4a', count: 0, isPlaying: false },
-        { id: 5, soundName: '5', soundURL: 'https://dl.dropboxusercontent.com/s/323rhnwp9bnglmj/struck-by-lightning.m4a', count: 0, isPlaying: false },
-        { id: 6, soundName: '6', soundURL: 'https://dl.dropboxusercontent.com/s/mh53hc4v5vfil2r/primal-noises.m4a', count: 0, isPlaying: false },
-        { id: 7, soundName: '7', soundURL: 'https://dl.dropboxusercontent.com/s/44wlc2ifqf20qzt/family.m4a', count: 0, isPlaying: false },
-        { id: 8, soundName: '8', soundURL: 'https://dl.dropboxusercontent.com/s/s70hruqm15jd7qc/lazer.m4a', count: 0, isPlaying: false },
-        { id: 9, soundName: '9', soundURL: 'https://dl.dropboxusercontent.com/s/tpdoeh03q26soil/stay-as-little-as-possible.m4a', count: 0, isPlaying: false },
-        { id: 10, soundName: '10', soundURL: 'https://dl.dropboxusercontent.com/s/ep463jwi2y99gfw/terrible.m4a', count: 0, isPlaying: false },
-        { id: 11, soundName: '11', soundURL: 'https://dl.dropboxusercontent.com/s/8tmikmnhr0k59bx/win-an-award.m4a', count: 0, isPlaying: false }
+        { id: 1, soundName: 'Boots', soundURL: 'https://www.pacdv.com/sounds/people_sound_effects/footsteps-3.mp3', count: 0, isPlaying: false }, 
+        { id: 2, soundName: 'Stumble', soundURL: 'http://www.flashkit.com/imagesvr_ce/flashkit/soundfx/People/Pain/Oowh_Pai-Nathan_H-7830/Oowh_Pai-Nathan_H-7830_hifi.mp3', count: 0, isPlaying: false },
+        { id: 3, soundName: 'Clank', soundURL: 'https://www.pacdv.com/sounds/mechanical_sound_effects/glass_breaking_2.wav', count: 0, isPlaying: false },
+        { id: 4, soundName: 'Dragon', soundURL: 'http://www.flashkit.com/imagesvr_ce/flashkit/soundfx/Interfaces/Roars/Vwoooaaa-Public_D-344/Vwoooaaa-Public_D-344_hifi.mp3', count: 0, isPlaying: false },
+        { id: 5, soundName: 'Market', soundURL: 'https://www.pacdv.com/sounds/people_sound_effects/hallway-crowd.mp3', count: 0, isPlaying: false },
+        { id: 6, soundName: 'Secret', soundURL: 'https://www.pacdv.com/sounds/domestic_sound_effects/matches-1.wav', count: 0, isPlaying: false },
+        { id: 7, soundName: 'Healing', soundURL: 'http://www.flashkit.com/imagesvr_ce/flashkit/soundfx/Interfaces/Energy-Mystery-8894/Energy-Mystery-8894_hifi.mp3', count: 0, isPlaying: false },
+        { id: 8, soundName: 'Monster', soundURL: 'http://www.flashkit.com/imagesvr_ce/flashkit/soundfx/Creatures/Other/Predator-Hotfoot_-8607/Predator-Hotfoot_-8607_hifi.mp3', count: 0, isPlaying: false },
+        { id: 9, soundName: 'Burgle', soundURL: 'https://www.pacdv.com/sounds/mechanical_sound_effects/coins_4.wav', count: 0, isPlaying: false },
+        { id: 10, soundName: 'Mercenary', soundURL: 'https://www.pacdv.com/sounds/voices/over-here-1.wav', count: 0, isPlaying: false },
       ]
     }
   }
